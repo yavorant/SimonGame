e7cfe033df91dec4e16780b737e6cf2d4d242921
yavorant <yavorant@gmail.com>
2021-04-15 15:44:35 +0300

    Sound worksgit add .!

--- game.js
+++ game.js
@@ -4,26 +4,47 @@
 let gamePattern = [];
 const buttonColours = ["red", "blue", "green", "yellow"];
 
-//a new random number between 0 and 3
-let randomNumber = function nextSequence(max = 4) {
-  return Math.floor(Math.random() * max);
-}
-// select a random color from the buttonColours array
-let randomChosenColour = () => buttonColours[randomNumber()];
+/*
+$("body").keydown(function() {
+  console.log("Handler for .keypress() called.");
+});
+*/
 
-gamePattern.push(randomChosenColour());
+
+    $("body").keypress(function(){
+        nextSequence();
+    });
+
+function nextSequence() {
+//a new random number between 0 and 3
+let randomNumber = Math.floor(Math.random() * 4);
+// select a random color from the buttonColours array
+let randomChosenColour = buttonColours[randomNumber];
+
+gamePattern.push(randomChosenColour);
 
 //console.log(gamePattern);
 
 //Use jQuery to select the button with the same id as the randomChosenColour
 
-let randomColor = randomChosenColour();
-console.log(randomColor);
-$(`#${randomColor}`).fadeOut().fadeIn();
-let currentColorSound = new Audio(`sounds/${randomColor}.mp3`);
-currentColorSound.play();
+
+$(`#${randomChosenColour}`).fadeOut(500).fadeIn(500);
+console.log(randomChosenColour);
+
+playSound(randomChosenColour);
+//let audio = new Audio("sounds/" + randomColor + ".mp3");
+//audio.play();
+//new Audio("sounds/red.mp3").play();
+
+//audio.play();
 
 //$(document.body).animate({opacity: '30%'}, 1000);
 //$(document.body).fadeOut().fadeIn();
-audioObj = new Audio(url);
-myAudioElement.play();
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+console.log(gamePattern);
